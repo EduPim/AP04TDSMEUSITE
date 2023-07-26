e0bf5259f9ccf3351378a149603ca84ebde4b8a3
Eduardo <du01pimentel@gmail.com>
2023-07-25 22:43:38 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>CatS Editor</h1>
+    <Cat></Cat>
+  </div>
+</template>
+
+<script>
+import Cat from './components/Cat'
+
+export default {
+  name: 'App',
+  components: {
+    Cat
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2b2b2b;
+  margin-top: 60px;
+  background-image: url('assets/CAIXAS.jpg');
+  background-size: 100%;
+}
+
+</style>
---
+++ src/components/Cat.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div id="selection">
+            <button id="button" type="button" class="btn" v-on:click="catSize++">BIG Cats</button>
+            <button id="button" type="button" class="btn" v-on:click="catSize--">SMALL Cats</button>
+            <button id="button" type="button" class="btn" @click="holder=Switch1(holder);">Change Cat 1</button>
+            <button id="button" type="button" class="btn" @click="holder=Switch2(holder);">Change Cat 2</button>
+            <button id="button" type="button" class="btn" @click="holder=Switch3(holder);">SURPRISE</button>
+        </div>
+        <div>
+            <img id="cat1" src="../assets/jinx2.gif" class="jinxcat" v-bind:style="{ width: catSize + '%',visibility: hidden }">
+            <img id="cat2" src="../assets/3dcat2.gif" class="jinxcat" v-bind:style="{ width: catSize + '%',visibility: hidden}">
+        </div>
+    </div>  
+</template>
+
+  <script>
+
+    export default {
+        methods:{
+            Switch1(){
+                document.getElementById('cat1').style.visibility = 'visible';
+                document.getElementById('cat2').style.visibility = 'hidden';
+            },
+            Switch2(){
+                document.getElementById('cat1').style.visibility = 'hidden';
+                document.getElementById('cat2').style.visibility = 'visible';
+            },
+            Switch3(){
+                document.getElementById('cat1').style.visibility = 'visible';
+                document.getElementById('cat2').style.visibility = 'visible';
+            },
+        },
+        sl: '#selection',
+        data () {
+            return {
+                catSize: 20,
+                hidden: "hidden"
+            }
+        }
+    }
+
+
+
+  </script>
+  <style scoped>
+  </style>
